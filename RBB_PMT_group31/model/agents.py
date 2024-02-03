06b9573ab0d0836141387c217824b0e2f604c073
fassland <151762456+fassland@users.noreply.github.com>
2024-02-03 10:52:52 +0100

    Add files via upload

---
+++ RBB_PMT_group31/model/agents.py
@@ -0,0 +1,64 @@
+# Importing necessary libraries
+import random
+from mesa import Agent
+from shapely.geometry import Point
+from shapely import contains_xy
+
+# Import functions from functions.py
+from functions import generate_random_location_within_map_domain, get_flood_depth, calculate_basic_flood_damage, floodplain_multipolygon
+
+
+# Define the Households agent class
+class Households(Agent):
+    """
+    An agent representing a household in the model.
+    Each household has a flood depth attribute which is randomly assigned for demonstration purposes.
+    In a real scenario, this would be based on actual geographical data or more complex logic.
+    """
+
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.is_adapted = False  # Initial adaptation status set to False
+
+        # getting flood map values
+        # Get a random location on the map
+        loc_x, loc_y = generate_random_location_within_map_domain()
+        self.location = Point(loc_x, loc_y)
+
+        # Check whether the location is within floodplain
+        self.in_floodplain = False
+        if contains_xy(geom=floodplain_multipolygon, x=self.location.x, y=self.location.y):
+            self.in_floodplain = True
+
+        # Get the estimated flood depth at those coordinates. 
+        # the estimated flood depth is calculated based on the flood map (i.e., past data) so this is not the actual flood depth
+        # Flood depth can be negative if the location is at a high elevation
+        self.flood_depth_estimated = get_flood_depth(corresponding_map=model.flood_map, location=self.location, band=model.band_flood_img)
+        # handle negative values of flood depth
+        if self.flood_depth_estimated < 0:
+            self.flood_depth_estimated = 0
+        
+        # calculate the estimated flood damage given the estimated flood depth. Flood damage is a factor between 0 and 1
+        self.flood_damage_estimated = calculate_basic_flood_damage(flood_depth=self.flood_depth_estimated,isadapted=self.is_adapted)
+
+        # Add an attribute for the actual flood depth. This is set to zero at the beginning of the simulation since there is not flood yet
+        # and will update its value when there is a shock (i.e., actual flood). Shock happens at some point during the simulation
+        self.flood_depth_actual = 0
+        
+        #calculate the actual flood damage given the actual flood depth. Flood damage is a factor between 0 and 1
+        self.flood_damage_actual = calculate_basic_flood_damage(flood_depth=self.flood_depth_actual,isadapted=self.is_adapted)
+
+        self.risk_appraisal = 0
+        self.coping_appraisal = 0
+        
+    
+    def step(self):
+        if random.random() < (self.risk_appraisal + self.coping_appraisal)/2: # if a random number between 0 and 1 is smaller than the average appraisal of a Household, it adapts
+            self.is_adapted = True  # Agent adapts to flooding
+    
+# # # # # Define the Government agent class
+class Government(Agent):
+    """
+    Please note that we assume government would impose the campaigns, however it was easiest to implement it without using this agent as such.
+    """
+    pass
